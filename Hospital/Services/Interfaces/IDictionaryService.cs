48034daecf95a969b0d0a426f54dea6955a4db00
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2024-01-07 15:31:13 +0700

    Сделал эндпоинт списка специальностей

---
+++ Hospital/Services/Interfaces/IDictionaryService.cs
@@ -0,0 +1,9 @@
+﻿using Hospital.Models.Speciality;
+
+namespace Hospital.Services.Interfaces
+{
+    public interface IDictionaryService
+    {
+        Task<SpecialtiesPagedListModel> GetSpecialitiesList(string? name, int page, int size);
+    }
+}
