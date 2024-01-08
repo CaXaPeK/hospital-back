1ff9a534f7974d938b5f63a5a36112dc26342f42
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2024-01-08 20:42:19 +0700

    Сделал GetPatient

--- Hospital/Services/Interfaces/IPatientService.cs
+++ Hospital/Services/Interfaces/IPatientService.cs
@@ -5,5 +5,7 @@ namespace Hospital.Services.Interfaces
     public interface IPatientService
     {
         Task<Guid> CreatePatient(PatientCreateModel newPatient);
+
+        Task<PatientModel> GetPatient(Guid id);
     }
 }
