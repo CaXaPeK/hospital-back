2e3839e067798fc8f9983ad70956fd66ca79e96c
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2024-01-11 18:59:33 +0700

    Сделал EditInspection

--- Hospital/Services/Interfaces/IInspectionService.cs
+++ Hospital/Services/Interfaces/IInspectionService.cs
@@ -7,5 +7,11 @@ namespace Hospital.Services.Interfaces
     public interface IInspectionService
     {
         public Task<InspectionModel> GetFullInspection(Guid inspectionId);
+
+        public Task EditInspection(Guid inspectionId, InspectionEditModel editedInspection, Guid doctorId);
+
+        public void ValidateCreateInspection(InspectionCreateModel newInspection, Patient patient);
+
+        public int MainDiagnosesCount(List<DiagnosisCreateModel> diagnoses);
     }
 }
