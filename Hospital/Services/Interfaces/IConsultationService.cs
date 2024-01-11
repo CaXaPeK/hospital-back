dd967f741452896debb62d5ed66b37954bd6de98
Саша Баздырев <sasha.bazdyrev2004@gmail.com>
2024-01-12 02:59:15 +0700

    Сделал GetConsultation

--- Hospital/Services/Interfaces/IConsultationService.cs
+++ Hospital/Services/Interfaces/IConsultationService.cs
@@ -1,9 +1,12 @@
 ﻿using Hospital.Models.Inspection;
+using Hospital.Models.Consultation;
 
 namespace Hospital.Services.Interfaces
 {
     public interface IConsultationService
     {
         Task<InspectionPagedListModel> GetYourSpecialityInspections(Guid doctorId, List<Guid> icdRoots, bool grouped, int page, int size);
+
+        Task<ConsultationModel> GetConsultation(Guid id);
     }
 }
